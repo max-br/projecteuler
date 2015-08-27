4e680bdfb51f5d9ffb334812315a3ff638a9ca65
max-br <max.breitenfeldt@gmail.com>
2015-08-27 15:10:39 +0200

    finished problem 27

--- problem27/main.go
+++ problem27/main.go
@@ -7,10 +7,47 @@ import (
 
 var primes []bool = lib.Primesieve(1000000)
 
+func isPrime(n int) bool {
+	return primes[n]
+}
+
 func evalExpr(a int, b int) (primecnt int) {
+	n := 0
+	candidate := n*n + a*n + b
+	for candidate > 0 && isPrime(candidate) {
+		primecnt++
+		n++
+		candidate = n*n + a*n + b
+	}
+	return
+}
+
+func testBValues(a int) (bmax int, maxcnt int) {
+	b := -999
+	for b < 1000 {
+		cnt := evalExpr(a, b)
+		if cnt > maxcnt {
+			bmax = b
+			maxcnt = cnt
+		}
+		b++
+	}
 	return
 }
 
 func main() {
 	fmt.Println()
+	amax, bmax, maxcnt := 0, 0, 0
+	a := -999
+	for a < 1000 {
+		b, cnt := testBValues(a)
+		if cnt > maxcnt {
+			amax = a
+			bmax = b
+			maxcnt = cnt
+		}
+		a++
+		fmt.Println(a, amax, bmax, maxcnt)
+	}
+	fmt.Println(amax * bmax)
 }
